5e02b9ecd86015f8967827abc3578b87166584a9
scottmcscran <tfgexstoryadmin@proton.me>
2025-11-27 21:44:51 +0000

    Show close button on mobile job details

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8923,7 +8923,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61217" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61304" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
