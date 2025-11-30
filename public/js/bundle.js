e599539cae71ff1f8121db885facd4d5a102d469
scottmcscran <tfgexstoryadmin@proton.me>
2025-11-30 16:59:03 +0000

    Mobile card tap fix (hopefully)

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9433,7 +9433,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53578" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54216" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
